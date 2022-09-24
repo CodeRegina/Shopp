399664b12c9a96e904902a431b0a8acc4820dbf3
Regina Miller <regina.miller@outlook.com>
2022-09-24 01:14:45 -0500

    added filtering logic

--- repositories/users.js
+++ repositories/users.js
@@ -44,36 +44,55 @@ class UsersRepository {
   }
 
   async getOne(id) {
-
     const records = await this.getAll();
     return records.find(record => record.id === id);
-
   }
 
   async delete(id) {
     const records = await this.getAll();
-
     const filteredRecords = records.filter(record => record.id !== id);
-
-    await this.writeAll(filteredRecords); 
+    await this.writeAll(filteredRecords);
   }
 
   async update(id, attrs) {
-    const records = await this.getAll(); 
+    const records = await this.getAll();
     const record = records.find(record => record.id === id);
 
     if (!record) {
-      throw new Error(`Record with id ${id} not found`); 
+      throw new Error(`Record with id ${id} not found`);
     }
 
     Object.assign(record, attrs);
     await this.writeAll(records);
   }
+
+  async getOneBy(filters) {
+    const records = await this.getAll();
+
+    for (let record of records) {
+      let found = true;
+
+      for (let key in filters) {
+        if (record[key] !== filters[key]) {
+          found = false;
+        }
+      }
+
+      if (found) {
+        return record;
+      }
+    }
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
-  await repo.update('fc423a2f', { password: 'mypassword' } );
+
+  const user = await repo.getOneBy({
+    laskdjflaksjdflakjsdflkajsdflkj: '123'
+  });
+
+  console.log(user);
 };
 
 test();
