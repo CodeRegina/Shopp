d0cc55b264fef31df15edc05585a14effc7d0459
CodeRegina <regina.ward94@outlook.com>
2022-09-28 17:40:41 -0500

    created html layout

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,11 +1,21 @@
+const layout = require('../layout'); 
+
 module.exports = () => {
-    return `
-      <div>
-        <form method="POST">
-          <input name="email" placeholder="email" />
-          <input name="password" placeholder="password" />
-          <button>Sign In</button>
-        </form>
-      </div>
-    `;
-  };
+  return layout({
+    content: `
+        <div>
+          Your id is: ${req.session.userId}
+          <form method="POST">
+            <input name="email" placeholder="email" />
+            <input name="password" placeholder="password" />
+            <input name="passwordConfirmation" placeholder="password confirmation" />
+            <button>Sign Up</button>
+          </form>
+        </div>
+  
+      `
+
+  });
+};
+
+
--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,14 +1,21 @@
-module.exports = ({ req }) => {
-    return `
-      <div>
-        Your id is: ${req.session.userId}
-        <form method="POST">
-          <input name="email" placeholder="email" />
-          <input name="password" placeholder="password" />
-          <input name="passwordConfirmation" placeholder="password confirmation" />
-          <button>Sign Up</button>
-        </form>
-    </div>
-    `;
-  };
+
+const layout = require('../layout'); 
+
+module.exports = () => {
+  return layout({
+    content: `
+        <div>
+          Your id is: ${req.session.userId}
+          <form method="POST">
+            <input name="email" placeholder="email" />
+            <input name="password" placeholder="password" />
+            <input name="passwordConfirmation" placeholder="password confirmation" />
+            <button>Sign Up</button>
+          </form>
+        </div>
+  
+      `
+
+  });
+};
 
