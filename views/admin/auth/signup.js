a69635b1fb153550ba21b345f0f64db2eb574973
CodeRegina <regina.ward94@outlook.com>
2022-09-28 23:00:45 -0500

    improved validation

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,21 +1,17 @@
+const layout = require('../layout');
 
-const layout = require('../layout'); 
-
-module.exports = () => {
+module.exports = ({ req }) => {
   return layout({
     content: `
-        <div>
-          Your id is: ${req.session.userId}
-          <form method="POST">
-            <input name="email" placeholder="email" />
-            <input name="password" placeholder="password" />
-            <input name="passwordConfirmation" placeholder="password confirmation" />
-            <button>Sign Up</button>
-          </form>
-        </div>
-  
-      `
-
+    <div>
+      Your id is: ${req.session.userId}
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <input name="passwordConfirmation" placeholder="password confirmation" />
+        <button>Sign Up</button>
+      </form>
+    </div>
+  `
   });
 };
-
